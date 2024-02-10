dfc81db9d783a2a0044b0143458cd8c489cad8b9
hamzatahirghauri <hamzatahirghauri@gmail.com>
2024-02-10 12:59:57 +0200

    Added untracked files

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -11,7 +11,8 @@ export interface Games {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform: Platform} []
+  parent_platforms: {platform: Platform} [];
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
@@ -22,21 +23,27 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Games[]>([]);
   const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
+
+    setLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {setGames(res.data.results);
+      setLoading(false);
+    })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false)
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
